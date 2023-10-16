c7943d32eef1afe5b17569bb54cc5586917f553c
Dr. Kyle Salcedo, PT, DPT <kylesalcedo@users.noreply.github.com>
2023-10-16 14:27:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Sets Counter
-    const setsCounter = document.getElementById("sets");
-    const incrementSetsButton = document.getElementById("incrementSets");
-    const decrementSetsButton = document.getElementById("decrementSets");
-
-    let setsCount = 0;
-
-    incrementSetsButton.addEventListener("click", function() {
-        setsCount++;
-        setsCounter.textContent = setsCount;
-    });
-
-    decrementSetsButton.addEventListener("click", function() {
-        if (setsCount > 0) {
-            setsCount--;
-            setsCounter.textContent = setsCount;
-        }
-    });
+    // ...
 
     // Reps Counter
-    const repsCounter = document.getElementById("reps");
-    const incrementRepsButton = document.getElementById("incrementReps");
-    const decrementRepsButton = document.getElementById("decrementReps");
-
-    let repsCount = 0;
-
-    incrementRepsButton.addEventListener("click", function() {
-        repsCount++;
-        repsCounter.textContent = repsCount;
-    });
-
-    decrementRepsButton.addEventListener("click", function() {
-        if (repsCount > 0) {
-            repsCount--;
-            repsCounter.textContent = repsCount;
-        }
-    });
+    // ...
 
     // Sets x Time Counter
     const timeCounter = document.getElementById("time");
     const incrementTimeButton = document.getElementById("incrementTime");
     const decrementTimeButton = document.getElementById("decrementTime");
-    const startTimerButton = document.getElementById("startTimer");
+    const toggleTimerButton = document.getElementById("toggleTimer");
+    const timerIcon = document.getElementById("timerIcon");
 
     let timeCount = 0;
     let timerInterval;
@@ -58,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    startTimerButton.addEventListener("click", function() {
+    toggleTimerButton.addEventListener("click", function() {
         if (!timerInterval) {
             timerInterval = setInterval(function() {
                 if (timeCount > 0) {
@@ -66,8 +35,15 @@ document.addEventListener("DOMContentLoaded", function() {
                     updateTimeDisplay();
                 } else {
                     clearInterval(timerInterval);
+                    timerInterval = null; // Reset the interval
+                    timerIcon.className = "far fa-play-circle"; // Change to Start icon
                 }
             }, 1000); // Decrease by 1 second per second
+            timerIcon.className = "far fa-pause-circle"; // Change to Pause icon
+        } else {
+            clearInterval(timerInterval);
+            timerInterval = null; // Reset the interval
+            timerIcon.className = "far fa-play-circle"; // Change to Start icon
         }
     });
 
