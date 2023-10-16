5f8625411503369d03237112c25d9f7241958ab9
Dr. Kyle Salcedo, PT, DPT <kylesalcedo@users.noreply.github.com>
2023-10-16 13:45:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Sets x Reps Counter
+    // Sets Counter
+    const setsCounter = document.getElementById("sets");
+    const incrementSetsButton = document.getElementById("incrementSets");
+    const decrementSetsButton = document.getElementById("decrementSets");
+
+    let setsCount = 0;
+
+    incrementSetsButton.addEventListener("click", function() {
+        setsCount++;
+        setsCounter.textContent = setsCount;
+    });
+
+    decrementSetsButton.addEventListener("click", function() {
+        if (setsCount > 0) {
+            setsCount--;
+            setsCounter.textContent = setsCount;
+        }
+    });
+
+    // Reps Counter
     const repsCounter = document.getElementById("reps");
     const incrementRepsButton = document.getElementById("incrementReps");
     const decrementRepsButton = document.getElementById("decrementReps");
@@ -17,48 +36,4 @@ document.addEventListener("DOMContentLoaded", function() {
             repsCounter.textContent = repsCount;
         }
     });
-
-    // Sets x Time Counter
-    const timeCounter = document.getElementById("time");
-    const incrementTimeButton = document.getElementById("incrementTime");
-    const decrementTimeButton = document.getElementById("decrementTime");
-
-    let timeCount = 0;
-
-    incrementTimeButton.addEventListener("click", function() {
-        timeCount++;
-        updateTimeDisplay();
-    });
-
-    decrementTimeButton.addEventListener("click", function() {
-        if (timeCount > 0) {
-            timeCount--;
-            updateTimeDisplay();
-        }
-    });
-
-    // Sets x Reps Counter
-    const setsRepsCounter = document.getElementById("setsReps");
-    const incrementSetsRepsButton = document.getElementById("incrementSetsReps");
-    const decrementSetsRepsButton = document.getElementById("decrementSetsReps");
-
-    let setsRepsCount = 0;
-
-    incrementSetsRepsButton.addEventListener("click", function() {
-        setsRepsCount++;
-        setsRepsCounter.textContent = setsRepsCount;
-    });
-
-    decrementSetsRepsButton.addEventListener("click", function() {
-        if (setsRepsCount > 0) {
-            setsRepsCount--;
-            setsRepsCounter.textContent = setsRepsCount;
-        }
-    });
-
-    function updateTimeDisplay() {
-        const minutes = Math.floor(timeCount / 60);
-        const seconds = timeCount % 60;
-        timeCounter.textContent = `${minutes}:${seconds.toString().padStart(2, "0")}`;
-    }
 });
